ca6f7e2150e09f2662b13d6cb363a63c599a3a57
Prithwish-2003 <ghoshprithwish586@gmail.com>
2024-03-22 17:46:19 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Sign_language',
+    version= '0.0.0',
+    author= 'Pouravi',
+    author_email= 'pouravighosh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
